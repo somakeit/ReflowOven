28bc950d9e5f8e252ac6560eaaa4e5920166eedb
Bracken Dawson <abdawson@gmail.com>
2015-01-17 19:19:50 +0000

    PWM engine working

--- ReflowOven.ino
+++ ReflowOven.ino
@@ -5,7 +5,12 @@
 Adafruit_MAX31855 thermocouple(CS);
 
 #define  HEATER_PIN  3
-#define  TARGET  0.0
+
+//heater PWM definitions
+void heater_pwm_machine();
+int heater_pwm = 80; //0 to 100, only 100 AC 0-crossings in 1s
+
+unsigned long last_report = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -18,22 +23,28 @@ void setup() {
 }
 
 void loop() {
-   Serial.print("Internal Temp = ");
-   Serial.println(thermocouple.readInternal());
+  if (millis() > last_report + 1000) {
+    last_report = millis();
+    double c = thermocouple.readCelsius();
+    if (isnan(c)) {
+      Serial.print("Something wrong with thermocouple!");
+    } else {
+      Serial.print("Temp: "); 
+      Serial.print(c);
+    }
+    Serial.print("    Internal: ");
+    Serial.println(thermocouple.readInternal());
+  }
+  
+  heater_pwm_machine();
+}
 
-   double c = thermocouple.readCelsius();
-   if (isnan(c)) {
-     Serial.println("Something wrong with thermocouple!");
-     digitalWrite(HEATER_PIN, LOW);
-   } else {
-     Serial.print("C = "); 
-     Serial.println(c);
-     if (c < TARGET) {
-       digitalWrite(HEATER_PIN, HIGH);
-     } else {
-       digitalWrite(HEATER_PIN, LOW);
-     }
-   }
-
-   delay(1000);
+void heater_pwm_machine() {
+  /* Call this frequnetly.
+   State machine to do 1Hz PWM of the heater. */
+  if ((millis() % 1000) / 10 < heater_pwm) {
+    digitalWrite(HEATER_PIN, HIGH);
+  } else {
+    digitalWrite(HEATER_PIN, LOW);
+  }
 }
