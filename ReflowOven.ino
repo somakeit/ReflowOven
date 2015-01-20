297a8238758625cce6473e5fb2b1e31812a38686
Bracken Dawson <abdawson@gmail.com>
2015-01-20 20:50:15 +0000

    Working PID & PWM prototype

--- ReflowOven.ino
+++ ReflowOven.ino
@@ -1,3 +1,4 @@
+#include <PID_v1.h>
 #include <SPI.h>
 #include "Adafruit_MAX31855.h"
 
@@ -8,9 +9,20 @@ Adafruit_MAX31855 thermocouple(CS);
 
 //heater PWM definitions
 void heater_pwm_machine();
-int heater_pwm = 80; //0 to 100, only 100 AC 0-crossings in 1s
+double heater_pwm = 0;
+
+//heater PID definitions
+double kp = 2;
+double ki = 5;
+double kd = 1;
+double target_temp = 20;
+double input_temp = 0;
+
+PID heater_pid(&input_temp, &heater_pwm, &target_temp, kp, ki, kd, DIRECT);
 
 unsigned long last_report = 0;
+unsigned long last_poll = 0; //TODO think about 50 day wrap
+#define  POLL_RATE  1000
 
 void setup() {
   Serial.begin(9600);
@@ -20,18 +32,27 @@ void setup() {
   
   pinMode(HEATER_PIN, OUTPUT);
   digitalWrite(HEATER_PIN, LOW);
+  
+  heater_pid.SetOutputLimits(0, 100);
+  heater_pid.SetMode(AUTOMATIC);
 }
 
 void loop() {
+  if (millis() > last_poll + POLL_RATE) {
+    input_temp = thermocouple.readCelsius();
+    if (isnan(input_temp)) {
+      heater_pwm = 0;
+      Serial.println("ERROR WITH THERMOCOUPLE");
+    } else {
+      heater_pid.Compute();
+    }
+  }
+    
+  
   if (millis() > last_report + 1000) {
     last_report = millis();
-    double c = thermocouple.readCelsius();
-    if (isnan(c)) {
-      Serial.print("Something wrong with thermocouple!");
-    } else {
-      Serial.print("Temp: "); 
-      Serial.print(c);
-    }
+    Serial.print("Temp: ");
+    Serial.print(input_temp);
     Serial.print("    Internal: ");
     Serial.println(thermocouple.readInternal());
   }
